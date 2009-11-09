068b9f4d6f52b787648f9d8cb1365ea9335f5b09
albertclass <albertclass@a94d7126-06ea-11de-b17c-0f1ef23b492c>
2009-11-09 05:41:04 +0000

    删Helplib

--- Helplib/Include/exception_report.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef TOOLLIB_EXPORTS
-#define TOOLLIB_API __declspec(dllexport)
-#else
-#define TOOLLIB_API __declspec(dllimport)
-#endif	//TOOLLIB_EXPORTS
-
-TOOLLIB_API void InstallCrashHandler();
